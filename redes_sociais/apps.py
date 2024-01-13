038665cda28d6fd45b115947db4dcc64704e9222
Marcus <omarcusvinicius96@gmail.com>
2024-01-13 14:11:39 -0500

    criando os apps

---
+++ redes_sociais/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RedesSociaisConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'redes_sociais'
--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=15, null=False, unique = True)
+    email = email = models.EmailField(unique=True)
+    foto = models.ImageField(upload_to="usuario/", null=True)
+    sobre = models.TextField()
