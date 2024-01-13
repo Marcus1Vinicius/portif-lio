ffccba873fa1159c0535a9873ef4fc16fc144d93
Marcus <omarcusvinicius96@gmail.com>
2024-01-13 14:57:18 -0500

    mais ajustes

--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Usuario
 
-# Create your views here.
+def index(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'index.html', {'usuarios': usuarios})
+
