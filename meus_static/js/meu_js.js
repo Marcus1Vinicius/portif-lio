566f28720ab32688b67a2e4f02456ca8dbc0c12e
Marcus <omarcusvinicius96@gmail.com>
2024-02-07 10:15:12 -0500

    adicionando animações e fazendo correções.

---
+++ meus_static/js/meu_js.js
@@ -0,0 +1,14 @@
+const elementos = document.querySelectorAll('.hidden')
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+elementos.forEach((elementos) => myObserver.observe(elementos))
