98a7328418db4866ab727ab62b2ccbf6b9fd5c7f
Marcus <omarcusvinicius96@gmail.com>
2024-03-04 21:20:35 -0500

    adicionando um after e hover nos links

--- meus_static/js/meu_js.js
+++ meus_static/js/meu_js.js
@@ -18,3 +18,26 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona um evento de clique para todos os links com a classe "links"
+    var links = document.querySelectorAll('.links');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Previne o comportamento padrão do link
+            
+            // Obtém o ID da seção a ser scrollada do atributo data-id do link
+            var targetId = this.getAttribute('data-id');
+            
+            // Obtém a posição da seção alvo
+            var targetSection = document.getElementById(targetId);
+            var targetPosition = targetSection.offsetTop;
+            
+            // Realiza o scroll suave até a seção alvo
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
