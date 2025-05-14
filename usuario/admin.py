dc4677359a5f11909d04f6f7a62f038a6ebb468d
Marcus1Vinicius <omarcusvinicius96@gmail.com>
2025-05-14 11:02:31 -0500

    mudança no admin

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Usuario, Skill, Projeto, Rede_Social
+from .models import Usuario, Skill, Projeto, Rede_Social, Diploma
 
-admin.site.register([Usuario, Skill, Projeto, Rede_Social])
+admin.site.register([Usuario, Skill, Projeto, Rede_Social, Diploma])
 
 
 # Register your models here.
